9d8be0f9b9696806dbc4be8d3c55690b0467a404
CartWheel <recaialperemek@gmail.com>
2021-03-04 15:24:46 +0300

    görüntüyü alma methodunda değişikliğe gidildi

--- resources/petImage.py
+++ resources/petImage.py
@@ -1,7 +1,6 @@
-import time
 import datetime
 from flask_restful import Resource, reqparse
-from werkzeug.datastructures import FileStorage
+from flask import request
 from flask_jwt_extended import (
     jwt_required,
     create_refresh_token,
@@ -24,26 +23,29 @@ def allowed_file(filename):
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
+def allowed_file2(filename):
+    return str(filename).rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
 class PetImageUpload(Resource):
     parser = reqparse.RequestParser()
     parser.add_argument(
-        "file", type=FileStorage, location='files', required=True, help="this field can not be left blank"
-    )
-    parser.add_argument(
-        "pet_type", type=str, required=True, help="this field can not be left blank"
-    )
+        "pet_type", type=str, required=True, help="this field can not be left blank")
 
     @classmethod
     @jwt_required
     def post(cls):
         data = cls.parser.parse_args()
-        if data and allowed_file(data["file"].filename):
+        img = request.files
+
+        if data and allowed_file(img['file'].filename):
             user_id = get_jwt_identity()
-            #burayı daha sonra gözden geçir
-            (dt, micro), utctime = datetime.datetime.utcnow().strftime('%Y%m%d%H%M%S.%f').split('.'), datetime.datetime.utcnow
+            # burayı daha sonra gözden geçir
+            (dt, micro), utctime = datetime.datetime.utcnow().strftime('%Y%m%d%H%M%S.%f').split(
+                '.'), datetime.datetime.utcnow
             micro_time = "%s.%03d" % (dt, int(micro))
 
-            image = pet_image_saving_directory(data["pet_type"], data['file'], user_id, micro_time)
+            image = pet_image_saving_directory(data["pet_type"], img['file'], user_id, micro_time)
             pet_image = pet_image_schema.load({'image_path': image['image_path'], 'user_id': user_id,
                                                })
             pet_image.save_to_db()
